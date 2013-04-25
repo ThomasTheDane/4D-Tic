5e7be46d7ac6fdac047299cbc04d1123f13dca35
Mac-I Crowell <macicrowell@gmail.com>
2013-04-25 13:50:42 -0400

    Added check for winning

--- 4DManager.py
+++ 4DManager.py
@@ -1,10 +1,64 @@
 import bigBoard
 import smallBoard
 
+win_combos =([0, 1, 2],
+			 [3, 4, 5], 
+			 [6, 7, 8],
+			 [0, 3, 6], 
+			 [1, 4, 7], 
+			 [2, 5, 8],
+			 [0, 4, 8],
+			 [2, 4, 6]) #All possible winning combinations
+
+
 class small_board():
-	def __init__(self):
-		self.smallBoardList = [" " for i in range(9)]
+	def __init__(self,posList=0):
+		if posList == 0:
+			self.smallBoardList = [" " for i in range(9)]
+		else:
+			self.smallBoardList = posList
+
+	def check_win_p(self):
+		for win_line in win_combos:
+			if self.smallBoardList[win_line[0]] != " " and self.smallBoardList[win_line[0]] == self.smallBoardList[win_line[1]] and self.smallBoardList[win_line[1]] == self.smallBoardList[win_line[2]]:
+				return self.smallBoardList[win_line[0]]
+		return None
+
+	def getPosList(self):
+		return self.smallBoardList
+
+
+class cube():
+	small_boards =([0,1,2,3,4,5,6,7,8],
+				 [9,10,11,12,13,14,15,16,17], 
+				 [18,19,20,21,22,23,24,25,26],
+				 [0,1,2,9,10,11,18,19,20], 
+				 [3,4,5,12,13,14,21,22,23], 
+				 [6,7,8,15,16,17,14,25,26],
+				 [0,3,6,9,12,15,18,21,24],
+				 [1,4,7,10,13,16,19,22,25],
+				 [2,5,8,11,14,17,10,23,26],
+				 [0,4,8,9,13,17,18,22,26],
+				 [2,4,6,11,13,15,20,22,24],
+				 [0,1,2,12,13,14,24,25,26],
+				 [6,7,8,12,13,14,18,19,20],
+				 [0,2,6,10,13,16,20,23,26],
+				 [2,5,8,10,13,16,18,21,24])
+	def __init__(self, boards):
+		self.poslist = boards[0].getPosList() + boards[1].getPosList() + boards[2].getPosList()
+		self.sBoards = []
+
+	def checkcubeVictory(self):
+		for board in cube.small_boards:
+			temp_list = []
+			for pos in board:
+				temp_list.append(self.poslist[pos])
+			bWin = small_board(temp_list).check_win_p()
+			if bWin != None:
+				return bWin
+		return None
 		
+
 class big_board(): #tic-tac-toe board object
 	def __init__(self):
 		self.bigBoardList = [small_board() for i in range(9)]
@@ -15,6 +69,19 @@ class big_board(): #tic-tac-toe board object
 	def getSpotSymbol(self, bigIndex, smallIndex):
 		return self.bigBoardList[bigIndex-1].smallBoardList[smallIndex-1]
 	
+	def checkWinner(self):
+		cube_list = []
+		for win_line in win_combos:
+			temp_list = []
+			for pos in win_line:
+				temp_list.append(self.bigBoardList[pos])
+			cube_result = cube(temp_list).checkcubeVictory()
+			if cube_result  != None:
+				return cube_result
+		return None
+
+
+
 	#prints out the board
 	def print_board(self):
 		print("#########################")
@@ -39,6 +106,7 @@ class big_board(): #tic-tac-toe board object
 		print("#       #       #       #")
 		print("#########################")
 		
+
 class aGame():
 	def __init__(self):
 		self.theBigBoard = big_board()
@@ -96,7 +164,7 @@ class aGame():
 		elif(self.playersType[self.currentTurn] == 'Monkey'):
 			self.makeMonkeyMove()
 		else:
-			print('Critical Mission Failure #1')
+			print('Critical Mission Failure #1.1 Subsection A')
 		
 	def makeAIMove(self):
 		makeMonkeyMove()
@@ -111,7 +179,17 @@ class aGame():
 			self.currentTurn += 1
 			if(self.currentTurn == self.numPlayers):
 				self.currentTurn = 0
-	
+			self.checkVictory()
+
+	def checkVictory(self):
+		result = self.theBigBoard.checkWinner()
+		if result != None:
+			self.gameIsGoing = False
+			self.Win(result)
+
+	def Win(self,winner):
+		print("The winner is: " + winner)
+
 	def getPlayerTypes(self):
 		isValidInput = False
 		self.playersType = []
@@ -136,4 +214,4 @@ class aGame():
 			
 theGame = aGame()
 theGame.startGame()
-#theGame.playTheGame()
+theGame.playTheGame()
